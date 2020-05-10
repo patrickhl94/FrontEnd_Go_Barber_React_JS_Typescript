c1515d0db241b9c0f2f45853cd83f6641f38c577
Patrick Lima <patrickhl94@hotmail.com>
2020-05-10 19:15:56 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import AppProvider from './context/index';
 
 import GlobalStyle from './syles/global';
 
-import SignIn from './pages/SignIn';
+import Routes from './routes/index';
 
 const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter>
       <AppProvider>
-        <SignIn />
+        <Routes />
       </AppProvider>
 
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 };
 
