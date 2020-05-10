8fc064056fc5eab139ebd3fd4530c82e20fed59c
Patrick Lima <patrickhl94@hotmail.com>
2020-05-10 19:16:15 -0300

    Updated styles page SignIn

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import backgroundImage from '../../assets/sign-in-background.png';
@@ -17,6 +17,29 @@ export const Content = styled.div`
   place-content: center;
   width: 100%;
   max-width: 700px;
+`;
+
+const appearFrontLeft = keyframes`
+from {
+ opacity: 0;
+ transform: translateX(-50px)
+}
+
+to {
+  opacity: 1;
+  transform: translateX(0px)
+ }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  place-content: center;
+  width: 100%;
+  max-width: 700px;
+
+  animation: ${appearFrontLeft} 1s;
 
   form {
     margin: 80px 0;
